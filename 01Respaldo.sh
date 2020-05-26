0611101219f239fa8621498a28f33ff0d9e159aa
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2020-05-26 19:14:52 -0400

    Add files via upload

--- 01Respaldo.sh
+++ 01Respaldo.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-input=( $( cat "/opt/eznas/TOCP.txt" ) )
-NASPath=$(cat /opt/eznas/mountrute.txt)
-ObjectivePath=$(cat /opt/eznas/localroute.txt)
+KeyPath="/root/Desktop/NAS/.iamhere"
+Paths="/root/Desktop/prueba2"
+NASPath="/root/Desktop/NAS"
+ObjectivePath="/root/Desktop"
 mkdir -p $ObjectivePath
-
-for (( i = 0; i < ${#input[@]}; i++ ))
-do
-	if [[ ${input[i]} != " " ]]; then 
-		echo "''" ${input[i]} "''"
-			file="$NASPath/${input[i]}"
+if test -f $KeyPath; then
+	echo "$FILE existeeee"   
+input="/root/Desktop/GitDo.txt"
+	while IFS= read -r line
+	do
+	  	echo "$line"
+		  file="$NASPath/$line"
 		if [[ -f $file ]]; then
 			cp "$file" "$ObjectivePath"
 		fi
 		if [[ -d $file ]]; then
 			cp -r "$file" "$ObjectivePath"
 		fi
-	fi
-
-done 
-    
+	done < "$input"
+    else 
+    echo "No existe"
+fi
 
